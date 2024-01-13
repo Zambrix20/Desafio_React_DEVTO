b01feef0860bb8089fc99a39a75df5695b0e575c
zambrix20 <companero20112011@hotmail.com>
2024-01-12 22:13:20 -0600

    Update index.html and DetailPost components

--- client/src/components/DetailPost/AsideLeft_DetailPost.jsx
+++ client/src/components/DetailPost/AsideLeft_DetailPost.jsx
@@ -2,6 +2,26 @@
 
 export default function AsideLeft_DetailPost() {
     return (
-        <div>AsideLeft_DetailPost</div>
+        <aside>
+            <div
+                className="flex flex-col items-center gap-8 mt-20"
+            >
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <img src="https://img.icons8.com/glyph-neue/64/1A1A1A/hearts.png" className="w-7 h-7 hover:rounded-full hover:bg-red-500/80" />
+                    <span>305</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <img src="https://img.icons8.com/sf-regular/48/1A1A1A/messaging-.png" className="w-7 h-7 hover:rounded-full hover:bg-orange-500/80" />
+                    <span>95</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <img src="https://img.icons8.com/sf-regular/48/1A1A1A/bookmark-ribbon.png" className="w-8 h-7 hover:rounded-full hover:bg-blue-500/80" />
+                    <span>651</span>
+                </div>
+                <div className=" flex items-center cursor-pointer hover:rounded-full hover:bg-gray-200/50 p-2">
+                    <img src="https://img.icons8.com/ios-filled/50/1A1A1A/ellipsis.png" className="w-7 h-7" />
+                </div>
+            </div>
+        </aside >
     )
 }
--- client/src/pages/DetailPost.jsx
+++ client/src/pages/DetailPost.jsx
@@ -1,7 +1,16 @@
+import AsideLeft_DetailPost from "../components/DetailPost/AsideLeft_DetailPost";
+import Main_DetailPost from "../components/DetailPost/Main_DetailPost";
+import AsideRight_DetailPost from "../components/DetailPost/AsideRight_DetailPost";
 
 export default function DetailPost({ task }) {
     return (
-        <>
-        </>
+        <div className="grid grid-cols-[20%_52%_24%] max-w-screen-xl mx-auto gap-4 text-black ">
+            {/* Aside 1 */}
+            <AsideLeft_DetailPost />
+            {/* Main */}
+            <Main_DetailPost />
+            {/* Aside 2 */}
+            <AsideRight_DetailPost />
+        </div>
     )
 }
